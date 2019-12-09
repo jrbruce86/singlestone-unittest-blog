6d1eaf91e1aa8ebdd14e190d823dd37d707b9190
Jake <jrbruce86@gmail.com>
2019-12-09 14:16:11 -0500

    Finish most of the tests

---
+++ src/test/java/jake/blog/inventoryApi/controllers/StoreInventoryControllerTest.java
@@ -0,0 +1,28 @@
+package jake.blog.inventoryApi.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreInventoryControllerTest {
+
+    @Test
+    void purchase() {
+    }
+
+    @Test
+    void getPurchase() {
+    }
+
+    @Test
+    void customers() {
+    }
+
+    @Test
+    void customerPurchases() {
+    }
+
+    @Test
+    void customerTotalSpent() {
+    }
+}
